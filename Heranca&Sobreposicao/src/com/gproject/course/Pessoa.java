bc0fafeae1b4d75834c7498c4eb03fdf75a022ca
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-07-28 15:56:42 -0300

    Implementado classe pai

---
+++ Heranca&Sobreposicao/src/com/gproject/course/Pessoa.java
@@ -0,0 +1,12 @@
+package com.gproject.course;
+
+public class Pessoa {
+	public String nome;
+	
+	public String getNome() {
+		return this.nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
