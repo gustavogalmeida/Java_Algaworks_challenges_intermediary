9a8b5c04cde33f4d7a715f8802b1b1f5cb75e0c3
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-07-29 17:03:41 -0300

    implementado segunda forma de passar objetos para array de contas

--- DesafioPolimorfismoEClassesAbstratas/src/com/gprojects/java/Main.java
+++ DesafioPolimorfismoEClassesAbstratas/src/com/gprojects/java/Main.java
@@ -10,14 +10,20 @@ public class Main {
 		Fornecedor forn1 = new Fornecedor("Coca Cola");
 		Fornecedor forn2 = new Fornecedor("Antartica");
 		
-		Conta[] contasJulho = new Conta[4];
+		// Forma 1 de passar todas contas para lista
+		ContaReceber cr1 = new ContaReceber(cli1, "Coca 600ml gustavo", 6, "28/07/2022");
+		ContaReceber cr2 = new ContaReceber(cli2, "Coca Cola Cafe otavio", 3, "29/07/2022");
+		ContaPagar cp1 = new ContaPagar(forn1, "Compra de 6 fardos coca", 450, "01/07/2022");
+		ContaPagar cp2 = new ContaPagar(forn2, "Compra antartica 1 fardo", 80, "05/07/2022");
+		Conta[] contas = new Conta[]{cr1, cr2, cp1, cp2}; // passando objetos
+		report.imprimir(contas);
 		
+		// Forma 2 de passar todas contas pra lista
+		Conta[] contasJulho = new Conta[4];		
 		contasJulho[0] = new ContaReceber(cli1, "Coca 600ml gustavo", 6, "28/07/2022");
 		contasJulho[1] = new ContaReceber(cli2, "Coca Cola Cafe otavio", 3, "29/07/2022");
-		
 		contasJulho[2] = new ContaPagar(forn1, "Compra de 6 fardos coca", 450, "01/07/2022");
 		contasJulho[3] = new ContaPagar(forn2, "Compra antartica 1 fardo", 80, "05/07/2022");
-		
 		report.imprimir(contasJulho);
 	}
 }
