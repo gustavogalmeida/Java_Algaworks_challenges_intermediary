347c45aa40234d77eef9c718afc8854380bd6b64
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-07-29 16:56:03 -0300

    Implementado novos testes

--- DesafioPolimorfismoEClassesAbstratas/src/com/gprojects/java/Main.java
+++ DesafioPolimorfismoEClassesAbstratas/src/com/gprojects/java/Main.java
@@ -2,7 +2,22 @@ package com.gprojects.java;
 
 public class Main {
 	public static void main(String[] args) {
-				
+		RelatorioContas report = new RelatorioContas();
+		
+		Cliente cli1 = new Cliente("Gustavo G A A");
+		Cliente cli2 = new Cliente("Otavio F");
+		
+		Fornecedor forn1 = new Fornecedor("Coca Cola");
+		Fornecedor forn2 = new Fornecedor("Antartica");
+		
+		Conta[] contasJulho = new Conta[4];
+		
+		contasJulho[0] = new ContaReceber(cli1, "Coca 600ml gustavo", 6, "28/07/2022");
+		contasJulho[1] = new ContaReceber(cli2, "Coca Cola Cafe otavio", 3, "29/07/2022");
+		
+		contasJulho[2] = new ContaPagar(forn1, "Compra de 6 fardos coca", 450, "01/07/2022");
+		contasJulho[3] = new ContaPagar(forn2, "Compra antartica 1 fardo", 80, "05/07/2022");
+		
+		report.imprimir(contasJulho);
 	}
-
 }
