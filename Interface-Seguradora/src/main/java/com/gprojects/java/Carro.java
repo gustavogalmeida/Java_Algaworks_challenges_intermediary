155664cf4a71ae0dd52b307d7cae16442e6c3f56
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-08-01 16:33:45 -0300

    ajustado classe carro que implementa a interface Seguravel

--- Interface-Seguradora/src/main/java/com/gprojects/java/Carro.java
+++ Interface-Seguradora/src/main/java/com/gprojects/java/Carro.java
@@ -1,5 +1,25 @@
 package com.gprojects.java;
 
-public class Carro {
+public class Carro implements Seguravel{
 
+	public double valorMercado;
+	public int anoFabricacao;
+	
+	public Carro(double valorMercado, int anoFabricacao) {
+		this.valorMercado = valorMercado;
+		this.anoFabricacao = anoFabricacao;
+	}
+
+	public String obterDescricao() {
+		return "Carro fabricado em " + this.anoFabricacao + " e com valor de mercado: " + this.valorMercado;
+	}
+
+	public double calcularValorApolice() {
+		double valorApolice = this.valorMercado * 0.04;
+		if (this.anoFabricacao < 2000) {
+			valorApolice *= 0.90;
+		}
+		return valorApolice;
+	}
+	
 }
