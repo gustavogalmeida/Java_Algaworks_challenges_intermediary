44dd25d4a484268aeff1ca44996bb559b0cf1fe1
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-08-01 16:54:55 -0300

    implementado métodos na classe Imóvel

--- Interface-Seguradora/src/main/java/com/gprojects/java/Imovel.java
+++ Interface-Seguradora/src/main/java/com/gprojects/java/Imovel.java
@@ -2,11 +2,22 @@ package com.gprojects.java;
 
 public class Imovel implements Seguravel{
 	
+	private double valorMercado;
+	private int areaConstruida;
+	
+	public Imovel(double valorMercado, int areaConstruida) {
+		this.valorMercado = valorMercado;
+		this.areaConstruida = areaConstruida;
+	}
+
 	public String obterDescricao() {
-		return "";
+		return "Imovel com area construida de " + this.areaConstruida + "m2 e valor de mercado R$ " + this.valorMercado;
 	}
 	
 	public double calcularValorApolice() {
-		return 0;
+		double valorApolice = this.valorMercado * this.valorMercado;
+		valorApolice += (this.areaConstruida * 0.05); 
+		
+		return valorApolice;
 	}
 }
