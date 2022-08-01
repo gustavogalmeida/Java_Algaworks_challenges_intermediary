00e9f408c0ae352e1cc040267236ea918845a706
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-08-01 16:33:54 -0300

    Criado classe imovel

---
+++ Interface-Seguradora/src/main/java/com/gprojects/java/Imovel.java
@@ -0,0 +1,12 @@
+package com.gprojects.java;
+
+public class Imovel implements Seguravel{
+	
+	public String obterDescricao() {
+		return "";
+	}
+	
+	public double calcularValorApolice() {
+		return 0;
+	}
+}
