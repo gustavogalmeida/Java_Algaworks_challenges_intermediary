d43b463829ac800335a657bbb4c92682d57e64d5
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-08-02 14:39:51 -0300

    criado exceção

---
+++ DesafioException/src/main/java/com/gprojects/java/OperacaoContaException.java
@@ -0,0 +1,14 @@
+package com.gprojects.java;
+
+public class OperacaoContaException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3018204555895105801L;
+	
+	public OperacaoContaException(String message) {
+		super(message);
+	}
+	
+}
