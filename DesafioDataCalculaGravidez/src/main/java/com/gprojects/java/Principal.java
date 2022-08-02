773581fbdd4fe3c5c5ac11c2a55a2e2ad10cf516
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-08-02 16:47:20 -0300

    Testes feitos e funcionando

--- DesafioDataCalculaGravidez/src/main/java/com/gprojects/java/Principal.java
+++ DesafioDataCalculaGravidez/src/main/java/com/gprojects/java/Principal.java
@@ -1,7 +1,18 @@
 package com.gprojects.java;
 
+import java.util.Scanner;
+
 public class Principal {
+
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Ola sou seu Dr Computador!");
+		System.out.print("Qual seu nome: ");
 		
+		String nome = scan.nextLine();
+		System.out.print("Data da ultima menstruação: ");
+		String data = scan.nextLine();
+		System.out.println();
+		CalculadoraGravidez.calcular(data);
 	}
 }
