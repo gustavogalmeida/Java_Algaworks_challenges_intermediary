8b0f20e408f646792724f435411e8f2868715c63
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2024-01-04 22:42:52 -0300

    Implementing initial tests

--- CollectionsDesafio/testes collections/src/Main.java
+++ CollectionsDesafio/testes collections/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        
+        // anteriormente eu poderia criar um array de um objeto dessa forma
+        Student[] arrayStudents = new Student[5];
+
+        // collections veio para resolver esse problema da seguinte forma
+        List v1Students = new ArrayList(); // mas será que essa é a melhor forma?
+
+        // ainda podemos melhorar essa collection implementando os tipos genericos GENERICS
+        List<Student> v2Students = new ArrayList<Student>();
+
     }
 }
