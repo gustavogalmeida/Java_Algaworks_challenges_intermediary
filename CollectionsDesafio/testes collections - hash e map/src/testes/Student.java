9f9d5b5a3da7a73ccbe151475f092e473ab7d98a
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2024-01-05 23:30:02 -0300

    upgrade my student

---
+++ CollectionsDesafio/testes collections - hash e map/src/testes/Student.java
@@ -0,0 +1,14 @@
+package testes;
+
+public class Student {
+
+    public Student (String name){
+        this.name = name;
+    }
+    String name;
+    int age;
+
+    public String getName() {
+        return name;
+    }
+}
