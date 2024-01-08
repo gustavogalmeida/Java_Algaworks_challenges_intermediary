12d7882e76820052faa31dc692203443fb795e72
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2024-01-07 22:32:20 -0300

    making it necessary to override the equals and hashcode methods

--- EqualsAndaHashCode/src/Main.java
+++ EqualsAndaHashCode/src/Main.java
@@ -3,8 +3,19 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args){
         Set<Student> students = DataBaseSimulator.findStudents();
-        for (Student s : students){
-            System.out.println("Nome: " + s.getName());
+
+        // sortear aluno
+        Student studentDrawn = new Student(1L, "Pedro");
+
+        if (students.contains(studentDrawn)){
+            System.out.println("Congratulations, " + studentDrawn + ", you won a gift!");
+        } else {
+            System.out.println("Student not registred in the database.");
         }
+
+        // IMPRIMIR TODOS
+        //for (Student s : students){
+        //    System.out.println("Nome: " + s.getName());
+        //}
     }
 }
