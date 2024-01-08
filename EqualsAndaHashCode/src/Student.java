705ea21916209e42e9f3226968423348c598052d
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2024-01-07 22:31:33 -0300

    Student adapted to not have duplicates

--- EqualsAndaHashCode/src/Student.java
+++ EqualsAndaHashCode/src/Student.java
@@ -1,23 +1,25 @@
 public class Student {
+    private Long registrationNumber;
     private String name;
     private Integer age;
-
-    public Student(String nome) {
-        this.name = nome;
+    public Student(Long registrationNumber, String name) {
+        this.name = name;
     }
-
+    public Long getRegistrationNumber() {
+        return registrationNumber;
+    }
+    public void setRegistrationNumber(Long registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
     public Integer getAge() {
         return age;
     }
-
     public void setAge(Integer age) {
         this.age = age;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String nome) {
         this.name = nome;
     }
