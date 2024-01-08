8adadd8680af32410f288bf52de5fa5a20eef5ab
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2024-01-07 22:31:39 -0300

    Student adapted to not have duplicates

--- EqualsAndaHashCode/src/DataBaseSimulator.java
+++ EqualsAndaHashCode/src/DataBaseSimulator.java
@@ -3,9 +3,9 @@ import java.util.HashSet;
 public class DataBaseSimulator {
     public static Set<Student> findStudents(){
         Set<Student> students = new HashSet<Student>();
-        students.add(new Student("Pedro"));
-        students.add(new Student("Thiago"));
-        students.add(new Student("João"));
+        students.add(new Student(1L, "Pedro"));
+        students.add(new Student(2L, "Thiago"));
+        students.add(new Student(3L, "João"));
         return students;
     }
 }
